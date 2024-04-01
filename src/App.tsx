72f58820ffb68655359275e22c79cf39250143f2
Noï van Mondfrans <noivanmondfrans@gmail.com>
2024-04-01 15:13:42 +0200

    Set up project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "about",
+    element: <div>About</div>,
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
