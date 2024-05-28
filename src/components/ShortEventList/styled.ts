b25f6a48323be8b8c7fe6cb545271451dcd5f32d
Noï van Mondfrans <noivanmondfrans@gmail.com>
2024-05-28 08:57:58 +0200

    Removed lit item decoration due to bug on certain mobile browsers

--- src/components/ShortEventList/styled.ts
+++ src/components/ShortEventList/styled.ts
@@ -1,6 +1,8 @@
 import styled from "@emotion/styled";
 
-export const List = styled.ul``;
+export const List = styled.ul`
+  list-style: none;
+`;
 
 export const Title = styled.h3`
   margin-bottom: 5px;
